4e17ece42d71df22056eecb59873718857346d21
hdfredl <dat.leef@gmail.com>
2023-10-23 21:44:23 +0200

    länder uppe

--- Enums/Countries.cs
+++ Enums/Countries.cs
@@ -2,5 +2,5 @@
 
 public enum Countries
 {
-
+    India, China, United_States, Indonesia, Pakistan, Brazil, Russia, Mexico, Japan, Vietnam, Turkey, Germany, United_Kingdom, France, South_Africa, Italy, Kenya, South_Korea, Colombia, Uganda, Argentina, Canada, Saudi_Arabia, Peru, Malaysia, Australia, Chile, Bolivia, United_Arab_Emirates, Israel, Switzerland, Bahrain
 }
--- Enums/EuropeanCountry.cs
+++ Enums/EuropeanCountry.cs
@@ -2,6 +2,6 @@
 
 public enum EuropeanCountry
 {
-
+    Austria, Belgium, Bulgaria, Croatia, CzechRepublic, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Poland, Portugal, Romania, Slovakia, Slovenia, Spain, Sweden
 
 }
